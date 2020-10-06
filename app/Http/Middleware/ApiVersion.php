6a6635696e2dd98ea57beef524bdbce33a01f65a
Brad Flood <bflood@keyholesoftware.com>
2020-10-06 10:53:02 -0700

    updates from composer cs-fix

--- app/Http/Middleware/ApiVersion.php
+++ app/Http/Middleware/ApiVersion.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\JsonResponse ;
-
-
 use Closure;
 
 class APIVersion
@@ -22,25 +19,23 @@ class APIVersion
      */
     public function handle($request, Closure $next)
     {
-
         $version_name = explode('_', $request->route()->getName())[0];
         if (!($version_name === 'v2' || $version_name === 'v3' ||  $version_name === 'v4')) {
             return $next($request);
         }
-        $routeV = substr($version_name, 1,1);
+        $routeV = substr($version_name, 1, 1);
 
-        if ($url_header = $request->header("v")) {
+        if ($url_header = $request->header('v')) {
             $requestV = $url_header;
-         } else if ($queryParam = $request->input("v")) {
-             $requestV = $queryParam;
-         } else {
-            return $next($request); // TODO:
-         }
-         if ($routeV != $requestV) {
-            return response("Not Found", 404);
-         }
+        } elseif ($queryParam = $request->input('v')) {
+            $requestV = $queryParam;
+        } else {
+            return $next($request);
+        }
+        if ($routeV != $requestV) {
+            return response('Not Found', 404);
+        }
 
-         // all good
         return $next($request);
     }
 }
