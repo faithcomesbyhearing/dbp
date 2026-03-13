42f0610740bf7552f2bed9010810e7cf2d36e59c
Victor Gonzalez <victor@fullstacklabs.co>
2026-03-13 10:42:38 -0500

    Task-75715 Password Reset - Internationalization (i18n) Support.

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,6 +3,9 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
+        var msgSuccess  = @json(trans('auth.password_reset_success'));
+        var msgMinLength = @json(trans('auth.password_min_length'));
+        var msgMismatch  = @json(trans('auth.passwords_do_not_match'));
         var form = $('#password-reset');
         form.submit(function (e) {
             e.preventDefault();
@@ -14,7 +17,7 @@
                     data: form.serialize(),
                     dataType: 'json',
                     success: function (data) {
-                        $( "#message-box" ).prepend( '<div class="alert alert-success has-text-centered">Your password has been reset</div>' );
+                        $( "#message-box" ).prepend('<div class="alert alert-success has-text-centered">' + msgSuccess + '</div>');
                         window.location.href = "https://www.faithcomesbyhearing.com/bible-brain";
                     }
                 });
@@ -22,12 +25,12 @@
         });
         function Validate() {
             $( "#error-box" ).empty();
-            if ($("#password").val().length < 7) {
-                $( "#message-box" ).prepend( '<div class="alert alert-error has-text-centered">Your password must be at least eight characters</div>' );
+            if ($("#password").val().length < 8) {
+                $( "#message-box" ).prepend('<div class="alert alert-error has-text-centered">' + msgMinLength + '</div>');
                 return false;
             }
             if($("#password").val() != $("#password-confirm").val()) {
-                $( "#message-box" ).prepend( '<div class="alert alert-error has-text-centered">Your passwords do not match</div>' );
+                $( "#message-box" ).prepend('<div class="alert alert-error has-text-centered">' + msgMismatch + '</div>');
                 return false;
             }
             return true;
@@ -40,28 +43,28 @@
 <div class="api-form-container">
     <div role="banner" class="hero-default hero-default--bible-brain">
       <div class="hero-default__text mt-0" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
-        <h1 class="txt-h2">{{__('Reset Password')}}</h1>
+        <h1 class="txt-h2">{{ __('auth.resetPassword') }}</h1>
       </div>
     </div>
-    <form id="password-reset" class="column is-half is-offset-one-quarter" method="POST" action="{{ route('v4_internal_user.password_reset', ['token' => $reset_request->token,'v' => 4,'key' => 'tighten_37518dau8gb891ub']) }}">
+    <form id="password-reset" class="column is-half is-offset-one-quarter" method="POST" action="{{ route('v4_internal_user.password_reset', ['token' => $reset_request->token,'v' => 4, 'key' => 'tighten_37518dau8gb891ub']) }}">
         @csrf
         <div id="message-box"></div>
         <input type="hidden" name="token_id" value="{{ $reset_request->token }}">
         <input type="hidden" name="email" value="{{ $reset_request->email ?? '' }}">
 
         <div class="full-col__input-wrapper mb-20">
-            <label class="default-form__label" for="new_password">{{ __('New Password') }}</label>
-            <input type="password" class="default-input w-input" maxlength="256" id="password" name="new_password" data-name="new_password" placeholder="Type your new password...." required value="{{ old('new_password') }}">
+            <label class="default-form__label" for="new_password">{{ __('auth.password') }}</label>
+            <input type="password" class="default-input w-input" maxlength="256" id="password" name="new_password" data-name="new_password" placeholder="{{ __('auth.ph_password') }}" required value="{{ old('new_password') }}">
             @if($errors->has('new_password')) <span class="help is-danger"><strong>{{ $errors->first('new_password') }}</strong></span> @endif
         </div>
         <div class="full-col__input-wrapper mb-20">
-            <label class="default-form__label" for="password-confirm">{{ __('Confirm Password') }}</label>
-            <input type="password" class="default-input w-input" maxlength="256" id="password-confirm" name="new_password_confirmation" data-name="new_password_confirmation" placeholder="Confirm your password...." required value="{{ old('new_password_confirmation') }}">
+            <label class="default-form__label" for="password-confirm">{{ __('auth.confirmPassword') }}</label>
+            <input type="password" class="default-input w-input" maxlength="256" id="password-confirm" name="new_password_confirmation" data-name="new_password_confirmation" placeholder="{{ __('auth.ph_password_conf') }}" required value="{{ old('new_password_confirmation') }}">
             @if($errors->has('new_password_confirmation'))<span class="help is-danger"><strong>{{ $errors->first('new_password_confirmation') }}</strong></span>@endif
         </div>
-        
+
         <div class="full-col__input-wrapper align-center">
-            <input type="submit" value="{{ __('Reset Password') }}" data-wait="Please wait..." class="btn-md btn--send mb-40 w-button"/>
+            <input type="submit" value="{{ __('auth.resetPassword') }}" data-wait="{{ __('auth.please_wait') }}" class="btn-md btn--send mb-40 w-button"/>
         </div>
     </form>
 </div>
